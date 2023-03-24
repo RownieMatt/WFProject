7b06bb6de11f19fbabd7d97526f83f54a3092c72
RownieMatt <basubasrowniematthew@gmail.com>
2023-03-23 22:48:41 -0600

    Added insert,update,delete codes.

--- WFProject/ABMCollege3Guys/Form1.Designer.cs
+++ WFProject/ABMCollege3Guys/Form1.Designer.cs
@@ -28,38 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.UserNL = new System.Windows.Forms.Label();
-            this.UserNTBox = new System.Windows.Forms.TextBox();
+            this.UNLabel = new System.Windows.Forms.Label();
+            this.UNTBox = new System.Windows.Forms.TextBox();
             this.PasswordTBox = new System.Windows.Forms.TextBox();
             this.PasswordL = new System.Windows.Forms.Label();
             this.NameTBox = new System.Windows.Forms.TextBox();
             this.NameL = new System.Windows.Forms.Label();
             this.ContactNTBox = new System.Windows.Forms.TextBox();
             this.ContactL = new System.Windows.Forms.Label();
+            this.DiverseB = new System.Windows.Forms.Button();
+            this.IDTBox = new System.Windows.Forms.TextBox();
+            this.IDLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // UserNL
+            // UNLabel
             // 
-            this.UserNL.AutoSize = true;
-            this.UserNL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.UserNL.Location = new System.Drawing.Point(50, 148);
-            this.UserNL.Name = "UserNL";
-            this.UserNL.Size = new System.Drawing.Size(81, 21);
-            this.UserNL.TabIndex = 0;
-            this.UserNL.Text = "Username";
+            this.UNLabel.AutoSize = true;
+            this.UNLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UNLabel.Location = new System.Drawing.Point(50, 197);
+            this.UNLabel.Name = "UNLabel";
+            this.UNLabel.Size = new System.Drawing.Size(81, 21);
+            this.UNLabel.TabIndex = 0;
+            this.UNLabel.Text = "Username";
             // 
-            // UserNTBox
+            // UNTBox
             // 
-            this.UserNTBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.UserNTBox.Location = new System.Drawing.Point(161, 145);
-            this.UserNTBox.Name = "UserNTBox";
-            this.UserNTBox.Size = new System.Drawing.Size(168, 29);
-            this.UserNTBox.TabIndex = 1;
+            this.UNTBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UNTBox.Location = new System.Drawing.Point(161, 194);
+            this.UNTBox.Name = "UNTBox";
+            this.UNTBox.Size = new System.Drawing.Size(168, 29);
+            this.UNTBox.TabIndex = 1;
             // 
             // PasswordTBox
             // 
             this.PasswordTBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.PasswordTBox.Location = new System.Drawing.Point(161, 209);
+            this.PasswordTBox.Location = new System.Drawing.Point(161, 258);
             this.PasswordTBox.Name = "PasswordTBox";
             this.PasswordTBox.Size = new System.Drawing.Size(168, 29);
             this.PasswordTBox.TabIndex = 3;
@@ -68,7 +71,7 @@
             // 
             this.PasswordL.AutoSize = true;
             this.PasswordL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.PasswordL.Location = new System.Drawing.Point(50, 212);
+            this.PasswordL.Location = new System.Drawing.Point(50, 261);
             this.PasswordL.Name = "PasswordL";
             this.PasswordL.Size = new System.Drawing.Size(76, 21);
             this.PasswordL.TabIndex = 2;
@@ -77,7 +80,7 @@
             // NameTBox
             // 
             this.NameTBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.NameTBox.Location = new System.Drawing.Point(475, 145);
+            this.NameTBox.Location = new System.Drawing.Point(475, 194);
             this.NameTBox.Name = "NameTBox";
             this.NameTBox.Size = new System.Drawing.Size(168, 29);
             this.NameTBox.TabIndex = 5;
@@ -86,7 +89,7 @@
             // 
             this.NameL.AutoSize = true;
             this.NameL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.NameL.Location = new System.Drawing.Point(364, 148);
+            this.NameL.Location = new System.Drawing.Point(364, 197);
             this.NameL.Name = "NameL";
             this.NameL.Size = new System.Drawing.Size(52, 21);
             this.NameL.TabIndex = 4;
@@ -95,7 +98,7 @@
             // ContactNTBox
             // 
             this.ContactNTBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ContactNTBox.Location = new System.Drawing.Point(475, 212);
+            this.ContactNTBox.Location = new System.Drawing.Point(475, 261);
             this.ContactNTBox.Name = "ContactNTBox";
             this.ContactNTBox.Size = new System.Drawing.Size(168, 29);
             this.ContactNTBox.TabIndex = 7;
@@ -104,25 +107,56 @@
             // 
             this.ContactL.AutoSize = true;
             this.ContactL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ContactL.Location = new System.Drawing.Point(344, 215);
+            this.ContactL.Location = new System.Drawing.Point(344, 264);
             this.ContactL.Name = "ContactL";
             this.ContactL.Size = new System.Drawing.Size(125, 21);
             this.ContactL.TabIndex = 6;
             this.ContactL.Text = "Contact Number";
+            // 
+            // DiverseB
+            // 
+            this.DiverseB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DiverseB.Location = new System.Drawing.Point(344, 310);
+            this.DiverseB.Name = "DiverseB";
+            this.DiverseB.Size = new System.Drawing.Size(80, 43);
+            this.DiverseB.TabIndex = 8;
+            this.DiverseB.Text = "button1";
+            this.DiverseB.UseVisualStyleBackColor = true;
+            // 
+            // IDTBox
+            // 
+            this.IDTBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.IDTBox.Location = new System.Drawing.Point(301, 148);
+            this.IDTBox.Name = "IDTBox";
+            this.IDTBox.Size = new System.Drawing.Size(168, 29);
+            this.IDTBox.TabIndex = 10;
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.IDLabel.Location = new System.Drawing.Point(255, 151);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(25, 21);
+            this.IDLabel.TabIndex = 9;
+            this.IDLabel.Text = "ID";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 567);
+            this.Controls.Add(this.IDTBox);
+            this.Controls.Add(this.IDLabel);
+            this.Controls.Add(this.DiverseB);
             this.Controls.Add(this.ContactNTBox);
             this.Controls.Add(this.ContactL);
             this.Controls.Add(this.NameTBox);
             this.Controls.Add(this.NameL);
             this.Controls.Add(this.PasswordTBox);
             this.Controls.Add(this.PasswordL);
-            this.Controls.Add(this.UserNTBox);
-            this.Controls.Add(this.UserNL);
+            this.Controls.Add(this.UNTBox);
+            this.Controls.Add(this.UNLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -133,13 +167,16 @@
 
         #endregion
 
-        private Label UserNL;
-        private TextBox UserNTBox;
+        private Label UNLabel;
+        private TextBox UNTBox;
         private TextBox PasswordTBox;
         private Label PasswordL;
         private TextBox NameTBox;
         private Label NameL;
         private TextBox ContactNTBox;
         private Label ContactL;
+        private Button DiverseB;
+        private TextBox IDTBox;
+        private Label IDLabel;
     }
 }
